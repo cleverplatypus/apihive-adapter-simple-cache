1488fbd152a20151f172a887a1e69cda879fc317
Nicola Dal Pont <nicola.dal.pont@egrowcery.com>
2025-08-19 17:57:44 +0930

    chore: fixed base path for docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/apihive-adapter-simple-cache/',
   title: "Simple Request Cache Adapter",
   description: "A simple, browser-oriented caching adapter for APIHive.",
   themeConfig: {
